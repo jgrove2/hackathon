c0beda7d6901199d7eb531443cfad88d7a191ae2
Lena <you@example.com>
2023-07-14 13:30:51 -0400

    carousel for home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Carousel } from "antd";
 import ProductList from "./components/ProductList.js";
 import Navbar from './components/Navbar';
+import Carousel from 'react-bootstrap/Carousel';
+import travelersumbrella from './components/images/travelersumbrella.jpg'
+import redshirt from './components/images/redshirt.jpeg'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-// const contentStyle = {
-//   width: "100%",
-//   color: "#fff",
-//   lineHeight: "160px",
-//   textAlign: "center",
-//   background: "#364d79",
-// };
+const contentStyle = {
+  width: "100%",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
 
 function Home({setCartItems}) {
   const [item, setItems] = useState([]);
@@ -25,28 +30,50 @@ function Home({setCartItems}) {
   }, []);
   console.log(item);
 
-  //   const renderSlider = () => {
-  //     <Carousel autoplay>
-  //       <div>
-  //         <h3 style={contentStyle}>1</h3>
-  //       </div>
-  //       <div>
-  //         <h3 style={contentStyle}>2</h3>
-  //       </div>
-  //       <div>
-  //         <h3 style={contentStyle}>3</h3>
-  //       </div>
-  //       <div>
-  //         <h3 style={contentStyle}>4</h3>
-  //       </div>
-  //     </Carousel>;
+  const carouselStyle= {
+    padding: "150px",
+    width : "600px",
+    border: "1px",
+    alignSelf: "center",
+  }
 
   return (
     <div>
-      {/* {renderSlider()} */}
-      <div>
+    <Container  style={{backgroundColor: "#D3D3D3"}}>
+      <Carousel style={{display: "flex",
+        alignItems: "center"}}>
+      <Carousel.Item interval={2000}>
+        <img
+        style={carouselStyle}
+          src={travelersumbrella}
+          alt="First slide"
+        />
+        <Carousel.Caption style={{color: "black"}}>
+          <h3>Travelers Umbrella</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+        style={carouselStyle}
+          src={redshirt}
+          alt="First slide"
+        />
+        <Carousel.Caption style={{color: "black"}} >
+          <h3>Travelers Shirt</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      
+    </Carousel>
+    </Container>
+      <Row>
+        <Col>
+        <div className="contentStyle">
         {item && (<ProductList items={item} setCartItems={setCartItems} />)}
       </div>
+        </Col>
+      </Row>
     </div>
   );
 }
